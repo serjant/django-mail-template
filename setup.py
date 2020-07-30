6072b63f60e3d645cb431e8642d706b4050731b6
Vicente Ramos <vramosga@gmail.com>
2020-07-30 16:54:18 -0300

    Increase version number and tag.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django_mail_template',
-    version='0.1.10',
+    version='0.1.11',
     packages=find_packages(),
     include_package_data=True,
     license='BSD 3-clause',
