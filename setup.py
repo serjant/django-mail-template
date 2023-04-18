df567ccfbd2133d3e6f97da9a365bbda70e98d2d
David Baum <780938+serjant@users.noreply.github.com>
2023-04-18 23:26:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
                 'and process configuration so is possible to change used '
                 'mail template of a process at run time.',
     install_requires=[
-          'django-ckeditor',
+          'django-ckeditor==6.0.0',
     ],
     long_description=README,
     long_description_content_type='text/markdown',
